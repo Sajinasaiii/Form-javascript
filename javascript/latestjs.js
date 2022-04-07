81e3eb59ebd469c95466195dd9a504e42e80e33e
Sajinasaiii <sajinatv1999@gmail.com>
2022-04-07 11:59:25 +0530

    new

--- javascript/latestjs.js
+++ javascript/latestjs.js
@@ -25,78 +25,92 @@ function validateForm()
     // let nextSibling = currentNode.nextElementSibling;
     // let Notvalid = document.getElementsByClassName("Notvalid");
 form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    validateForm();
+        e.preventDefault();
+        validateForm();
 });
 if (fname.value.trim() == "") 
     {
         fname.nextElementSibling.innerHTML="First Name must be filled out";
         fname.className="highlight";
+        fname.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else if(!isNaN(fname.value))
     {
         fname.nextElementSibling.innerHTML="First Name name should not be number";
         fname.className="highlight";
+        fname.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else if(!fname.value.match(name))
     {
         fname.nextElementSibling.innerHTML="Name only allowed alphabets";
         fname.className="highlight";
+        fname.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else
     {
         fname.nextElementSibling.innerHTML="";
         fname.className="highlightnew";
+        fname.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
     }
 if (lname.value.trim() == "") 
     {
         lname.nextElementSibling.innerHTML="Last Name must be filled out";
         lname.className="highlight";
+        lname.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else if(!isNaN(lname.value))
     {
         lname.nextElementSibling.innerHTML="Last name should not be number";
         lname.className="highlight";
+        lname.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else if(!lname.value.match(name))
     {
         lname.nextElementSibling.innerHTML="Name only allowed alphabets";
         lname.className="highlight";
+        lname.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else
     {
         lname.nextElementSibling.innerHTML="";
         lname.className="highlightnew";
+        lname.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
     }
 if (emailnew.value.trim() == "") 
     {
         emailnew.nextElementSibling.innerHTML="Email Id must be filled out";
         emailnew.className="highlight";
+        emailnew.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else if(!emailnew.value.match(validemailid))
 {
         emailnew.nextElementSibling.innerHTML="Email not valid";
         emailnew.className="highlight";
+        emailnew.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
 }
 else
     {
         emailnew.nextElementSibling.innerHTML="";
         emailnew.className="highlightnew";
+        emailnew.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
     }
 if (address.value.trim() == "") 
     {
         address.nextElementSibling.innerHTML="Adress must be filled out";
         address.className="highlight";
+        address.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else if((address.value.length < 15) || (address.value.length > 100))
     {
         address.nextElementSibling.innerHTML="Adress must be 15-100 characters length";
         address.className="highlight";
+        address.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else
     {
         address.nextElementSibling.innerHTML="";
         address.className="highlightnew";
+        address.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
     }
 if (language1.checked==false && language2.checked==false && language3.checked==false) 
     {
@@ -110,11 +124,13 @@ if (birth.value == "")
     {
         birth.nextElementSibling.innerHTML="DOB must be filled out";
         birth.className="highlight";
+        birth.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else
     {
         birth.nextElementSibling.innerHTML="";
         birth.className="highlightnew";
+        birth.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
     }
 if (gen1.checked==false && gen2.checked==false ) 
     {
@@ -128,28 +144,35 @@ if (number.value.trim() == "")
     {
         number.nextElementSibling.innerHTML="Mobile number must be filled out";
         number.className="highlight";
+        number.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else if(!number.value.match(validmobile))
     {
         number.nextElementSibling.innerHTML="Number not valid";
         number.className="highlight";
+        number.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else
     {
         number.nextElementSibling.innerHTML="";
         number.className="highlightnew";
+        number.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
     }
-    if (password1.value.trim() == "") 
+if (password1.value.trim() == "") 
     {
         password1.nextElementSibling.innerHTML="Password must be filled out";
         password1.className="highlight";
         password2.className="highlight";
+        password1.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
+        password2.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
-    else if(!password1.value.match(password)||!password2.value.match(password))
+else if(!password1.value.match(password)||!password2.value.match(password))
     {
         password2.nextElementSibling.innerHTML="Password which must include one small,capital,special letters";
         password1.className="highlight";
         password2.className="highlight";
+        password1.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
+        password2.parentElement.querySelector('.fa-circle-exclamation').classList.add('icon');
     }
 else
     {
@@ -157,6 +180,8 @@ else
         password2.nextElementSibling.innerHTML="";
         password1.className="highlightnew";
         password2.className="highlightnew";
+        password1.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
+        password2.parentElement.querySelector('.fa-circle-exclamation').classList.remove('icon');
     }
 if (file.value=="") 
     {
@@ -167,6 +192,11 @@ else
         file.nextElementSibling.innerHTML="";
     }
 }
+// function icon(input)
+// {
+//     let feildDiv=input.parentElement;
+//     feildDiv.className="feildDiv success";
+// }
 
 
     
