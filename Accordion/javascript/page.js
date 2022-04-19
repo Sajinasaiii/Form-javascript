7d7d973b2bc0ea4bcedcc1a04e99898e204a4627
Sajinasaiii <sajinatv1999@gmail.com>
2022-04-19 16:08:41 +0530

    new

--- Accordion/javascript/page.js
+++ Accordion/javascript/page.js
@@ -10,15 +10,15 @@ $(document).ready(function () {
     $(".show").click(function () {
         if ($(this).hasClass("visible")) {
             $(this).removeClass("visible").next().slideUp();
-            $(this).children("i").removeClass("fa-circle-chevron-up");
-            $(this).children("i").addClass("fa-circle-chevron-down");
+            $(this).find("i").removeClass("fa-circle-chevron-up");
+            $(this).find("i").addClass("fa-circle-chevron-down");
         } else {
             $(".show").removeClass("visible").next().slideUp();
-            $(".show").children("i").removeClass("fa-circle-chevron-up");
-            $(this).children("i").addClass("fa-circle-chevron-down");
+            $(".show").find("i").removeClass("fa-circle-chevron-up");
+            $(".show").find("i").addClass("fa-circle-chevron-down");
             $(this).addClass("visible").next().slideDown();
-            $(this).children("i").removeClass("fa-circle-chevron-down");
-            $(this).children("i").addClass("fa-circle-chevron-up");
+            $(this).find("i").removeClass("fa-circle-chevron-down");
+            $(this).find("i").addClass("fa-circle-chevron-up");
         }
     });
 });
