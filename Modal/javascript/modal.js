3d899184e5b4d1a22fd7b5e3eb6aa920a68c1c0f
Sajinasaiii <sajinatv1999@gmail.com>
2022-04-20 09:47:08 +0530

    new

--- Modal/javascript/modal.js
+++ Modal/javascript/modal.js
@@ -1,13 +1,12 @@
-$(document).ready(function()
-{
- $(".button").click(function(){
-    $(this).hide();
-    $(".main-content").addClass("visible");
-    $(".content").addClass("visible");
- });
- $(".button1").click(function(){
-    $(".button").show();
-    $(".main-content").removeClass("visible");
-    $(".content").removeClass("visible");
- });
+$(document).ready(function () {
+   $(".button").click(function () {
+      $(this).hide();
+      $(".main-content").addClass("visible");
+      $(".content").addClass("visible");
+   });
+   $(".button1").click(function () {
+      $(".button").show();
+      $(".main-content").removeClass("visible");
+      $(".content").removeClass("visible");
+   });
 });
