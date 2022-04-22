a633ee96605ad687f41cbebfb916d6e2141c6ed9
Sajinasaiii <sajinatv1999@gmail.com>
2022-04-22 18:33:05 +0530

    new

---
+++ Mynthra/js/new.js
@@ -0,0 +1,6 @@
+var el_down = document.getElementById("GFG_DOWN");
+function Geeks() {
+    var tb = document.getElementById("thead");
+    tb.style.display = "none";
+    el_down.innerHTML = "Table header is hidden";
+}
